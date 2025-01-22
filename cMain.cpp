e0e16c041ad53f557731f5bd278a2f8854b7cbdf
lecture <ksw617@gmail.com>
2025-01-22 20:05:09 +0900

    Initial commit

---
+++ cMain.cpp
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <Windows.h>
+#pragma region Enum
+enum Color
+{
+	Black,
+	DarkBlue,
+	DarkGreen,
+	DarkCyan,
+	DarkRed,
+	DarkMagenta,
+	DarkYellow,
+	Gray,
+	DarkGray,
+	Blue,
+	Green,
+	Cyan,
+	Red,
+	Magenta,
+	Yellow,
+	White,
+};
+#pragma endregion
+
+#pragma region WIN_API
+void HideCursor();
+void SetPosition(int x, int y);
+void ChangeColor(Color color);
+#pragma endregion
+
+int main()
+{
+	HideCursor();
+
+	while (true)
+	{
+		system("cls");
+
+		SetPosition(10, 10);
+		ChangeColor(Yellow);
+		printf("Hello world");
+
+		Sleep(50);
+
+	}
+
+	return 0;
+}
+
+
+#pragma region WIN_API
+//커서 이동
+void SetPosition(int x, int y)
+{
+	COORD pos;
+	pos.X = x * 2;
+	pos.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+//커서 보이는거 방지
+void HideCursor()
+{
+	CONSOLE_CURSOR_INFO info;
+	info.bVisible = false;
+	info.dwSize = 1;
+
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
+}
+
+//색깔 바꾸기
+void ChangeColor(Color color)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+#pragma endregion
